4668b4c5603ea345cc2edb00e5d743769be40c5b
Jayashakthi28 <vishnu28.js@gmail.com>
2021-06-19 17:37:37 +0530

    Fixed Some bugs

--- 1-drums-kit/script.js
+++ 1-drums-kit/script.js
@@ -29,6 +29,9 @@ let mapper={
 let key;
 let flag=0;
 let snd_record=[];
+let button=document.querySelector('.rec-stp');
+let key_list=document.querySelectorAll('.cont');
+let exp=document.querySelector('.btn-info-show');
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -36,6 +39,27 @@ function sleep(milliseconds) {
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
   }
+ function main_player(keys){
+    keyer=document.querySelector(`.cont[data-key="${keys}"]`);
+    let snd=document.querySelector(`audio[data-key=${mapper[keys]}]`);
+    if(!snd) return;
+    sleep(200);
+    snd.currentTime=0;
+    snd.play();
+    let child=keyer.childNodes;
+    child[1].classList.add('playing');
+    keyer.classList.add('playing-bg');
+}
+function record_player(){
+if(snd_record.length!=0){
+snd_record.forEach(temp=>
+    main_player(temp));
+}
+}
+key_list.forEach(keys => keys.addEventListener("transitionend",function(e){
+    keys.children[0].classList.remove('playing');
+    keys.classList.remove('playing-bg');
+}))
 window.addEventListener("keydown",function(e){
     if(flag != 0){
         snd_record.push(e.keyCode);
@@ -49,51 +73,21 @@ window.addEventListener("keydown",function(e){
     child[1].classList.add('playing');
     key.classList.add('playing-bg');
 })
-let key_list=document.querySelectorAll('.cont');
-key_list.forEach(keys => keys.addEventListener("transitionend",function(e){
-if(e.propertyName != "box-shadow") return;
-    keys.childNodes[1].classList.remove('playing');
-    keys.classList.remove('playing-bg');
-}))
-let button=document.querySelector('.rec-stp');
-function delayer(){
-    setTimeout(function(){
-        console.log("Entering into pause..");
-    },3000);
-}
-function main_player(keys){
-            console.log(keys);
-            keyer=document.querySelector(`.cont[data-key="${keys}"]`);
-            let snd=document.querySelector(`audio[data-key=${mapper[keys]}]`);
-            if(!snd) return;
-            sleep(200);
-            snd.currentTime=0;
-            snd.play();
-            let child=keyer.childNodes;
-            console.log(keyer);
-            console.log(child[1]);
-            child[1].classList.add('playing');
-            keyer.classList.add('playing-bg');
-}
-function record_player(){
-    console.log("Entered");
-    console.log(snd_record);
-    if(snd_record.length!=0){
-        console.log("Entered if");
-        snd_record.forEach(temp=>
-            main_player(temp));
-    }
-}
 button.addEventListener("click",function(e){
-    if(button.textContent==="Record"){
+    if(button.textContent=="Record"){
         flag=1;
         button.textContent="Stop and Play";
+        exp.textContent="Please Wait...";
     }
     else{
         flag=0;
         button.textContent="Record";
+        exp.textContent="Just click Record Button to Record";
         record_player();
         snd_record=[];
     }
 })
+
+
+
 
