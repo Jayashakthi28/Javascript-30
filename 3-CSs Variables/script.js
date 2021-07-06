2c619480609baaad649eb94c4d7d8e0ae308c1bc
Jayashakthi28 <vishnu28.js@gmail.com>
2021-07-06 10:34:31 +0530

    Fixed some bugs in the code

--- 3-CSs Variables/script.js
+++ 3-CSs Variables/script.js
@@ -3,5 +3,5 @@ function handle() {
   const suffix=this.dataset.size || '';
   document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
 }
-inputs.forEach((inputer) => inputer.addEventListener("change", handle));
-inputs.forEach((inputer) => inputer.addEventListener("mousemove", handle));
+inputs.forEach((inputer) => inputer.addEventListener("input", handle));
+// inputs.forEach((inputer) => inputer.addEventListener("mousemove", handle));
