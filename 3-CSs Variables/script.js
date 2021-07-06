efbc145e2aca6a0d43ab0fef4ed5671726c5856b
Jayashakthi28 <vishnu28.js@gmail.com>
2021-07-06 07:30:03 +0530

    Fixed some bugs in the code

--- 3-CSs Variables/script.js
+++ 3-CSs Variables/script.js
@@ -1,9 +1,6 @@
 var inputs = document.querySelectorAll("input");
-console.log(inputs);
 function handle() {
   const suffix=this.dataset.size || '';
-  console.log(this.name);
-  console.log(this.value);
   document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
 }
 inputs.forEach((inputer) => inputer.addEventListener("change", handle));
